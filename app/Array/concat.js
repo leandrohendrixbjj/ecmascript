00bcc9ad2ef7a18779582c5b120d9e40c907d24f
hendrix <leandrotully@gmail.com>
2022-12-10 15:08:37 -0300

    Concat

--- app/Array/concat.js
+++
@@ -1,9 +0,0 @@
-const suzuki = ["vmax","vstrom"];
-
-const triump = ["Tiger","Trident"];
-   
-const motos = suzuki.concat(triump);
-
-for(let moto in motos){
-    console.log(motos[moto]);
-}
---
+++ app/DataStructures/Array/Concat/1.js
@@ -0,0 +1,11 @@
+// O método concat() retorna um novo array contendo todos 
+// os arrays ou valores passados como parâmetro.
+console.clear();
+
+const suzuki = ["vmax", "vstrom"];
+const triump = ["Tiger", "Trident"];
+
+const motos = suzuki.concat(triump);
+
+console.log(motos);
+
