8b25bda6ba732834076dcc57e6ac17e628828ec7
hendrix <leandrotully@gmail.com>
2022-12-14 18:16:27 -0300

    Slice

--- app/DataStructures/Array/Slice/2.js
+++ app/DataStructures/Array/Slice/2.js
@@ -1,9 +1,7 @@
 console.clear();
 // Os arguments dentro de uma função são um exemplo de 'objeto array-like'.
-
 function list() {
     return Array.prototype.slice.call(arguments)
-
 }
 
 const data = list(1, 2);
