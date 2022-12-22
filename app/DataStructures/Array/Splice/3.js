ecfc316f6c91a69d4078d1e35e91ee1c1852e63c
hendrix <leandrotully@gmail.com>
2022-12-22 07:39:17 -0300

    Splice

---
+++ app/DataStructures/Array/Splice/3.js
@@ -0,0 +1,10 @@
+console.clear();
+// splice => Emendar
+
+//Clear array from index information
+
+const arr = ["angel", "clown", "mandarin", "surgeon"];
+
+arr.splice(-4);
+
+console.log(arr);
---
+++ app/DataStructures/Array/Splice/4.js
@@ -0,0 +1,10 @@
+console.clear();
+// splice => Emendar
+
+//if we use a deleteCount great that array length, it will remove all data from index
+
+const arr = ["angel", "clown", "mandarin", "surgeon"];
+
+arr.splice(1, 200);
+
+console.log(arr);
