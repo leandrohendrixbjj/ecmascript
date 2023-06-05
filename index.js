0e2c8d95aac4c0d84d70e5784906d1f9743d7a17
hendrix <leandrotully@gmail.com>
2023-06-05 08:25:33 -0300

    Order by desc objeto

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ console.clear();
 
 const customers = require('./helper.json');
 
-function orderBy(list, fieldName) {
-    return list.sort((a, b) => {
+function orderBy(list, fieldName, order = 'asc') {
+    const result = list.sort((a, b) => {
         if (a[fieldName] < b[fieldName]) return -1
         if (a[fieldName] > b[fieldName]) return 1
         return 0
     })
+
+    if (order == 'desc')
+        return result.reverse();
+
+    return result
 }
 
-console.log(
-    orderBy(customers, "nome")
-);
+const data = orderBy(customers, "nome", 'desc');
+
+console.log(data);
