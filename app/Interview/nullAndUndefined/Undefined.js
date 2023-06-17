94a56a2d279699afff1752b9a451f3a39828140e
hendrix <leandrotully@gmail.com>
2023-06-17 10:30:29 -0300

    Null and Undefined

---
+++ app/Interview/nullAndUndefined/Undefined.js
@@ -0,0 +1,8 @@
+console.clear();
+
+// Undefined => As variáveis que não possuem valor o JS as determina como undefined.
+// Em uma operação matemática com undefined o resultado será NaN.
+
+let age;
+
+console.log(age, age + 3, `Minha iadade é ${age}`);
---
+++ app/Variaveis/wTypes/Symbol/5.js
@@ -0,0 +1,17 @@
+console.clear();
+
+class Customer {
+    constructor(name) {
+        this.id = Symbol(),
+            this.name = name
+    }
+}
+
+const customers = [
+    new Customer('leandro'), new Customer('soares'), new Customer('ribeiro')]
+
+const n = customers[1];
+
+const customer = customers.filter((item) => item.id == n.id);
+
+console.log(customer);
