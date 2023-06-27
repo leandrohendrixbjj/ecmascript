1f48da1640f5e2cbc788264b6b315844aef0c78d
hendrix <leandrotully@gmail.com>
2023-06-27 20:14:09 -0300

    Arrow Funcion

--- app/Interview/FunctionAndArrow/1.js
+++ app/Interview/FunctionAndArrow/1.js
@@ -1,5 +1,6 @@
 // 1-) Principal diferença entre elas é a sintaxe
 // 2-) Arrows precisam está associadas a uma variável
+// 3-) Arrows permite retornar valores sem usar a palavra chave return
 
 console.clear();
 
@@ -10,6 +11,3 @@ function sum(a, b) {
     return a + b
 }
 console.log("Function", sum(1, 2));
-
-
-
