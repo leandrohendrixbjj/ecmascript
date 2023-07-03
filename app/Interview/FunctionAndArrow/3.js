8d8a57188f6f3c9f83c9ef7526e32c52269e87f5
hendrix <leandrotully@gmail.com>
2023-07-03 20:06:24 -0300

    Arrow Function

--- app/Interview/FunctionAndArrow/3.js
+++ app/Interview/FunctionAndArrow/3.js
@@ -1,12 +1,12 @@
-// 1-) Arrow: quando usamos new ela sempre vai considerar o this do objeto que 
-// foi invocado.
+// 1-) setInterval é uma função global o seu this pertence é this do obj window
+// 2-) Isso acontence quando usamos functions
 
 console.clear();
 
 function Person() {
     this.age = 0
 
-    setInterval(() => {
+    setInterval(function () {
         this.age = this.age + 1
         console.log('Qual é a idade', this.age)
     }, 1000)
---
+++ app/Interview/FunctionAndArrow/4.js
@@ -0,0 +1,15 @@
+// 1-) setInterval é uma função global o seu this pertence é this do obj window
+
+console.clear();
+
+function Person() {
+    that = this; // Isso é um modo elegante de resolver o 'problema' anterior
+    this.age = 0
+
+    setInterval(function () {
+        that.age = that.age + 1
+        console.log('Qual é a idade', that.age)
+    }, 1000)
+}
+const p = new Person()
+
